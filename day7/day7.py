32dd829d06844e483547deaa09197340e4ad8c7a
tailaiw <29800495+tailaiw@users.noreply.github.com>
2021-12-06 23:18:13 -0600

    reformat

--- day7/day7.py
+++ day7/day7.py
@@ -9,17 +9,7 @@ if __name__ == "__main__":
     s = [int(i) for i in input_data.split(",")]
 
     # Part 1
-    min_dist = float("inf")
-    for i in range(min(s), max(s) + 1):
-        dist = sum([abs(v - i) for v in s])
-        if dist < min_dist:
-            min_dist = dist
-    print(min_dist)
+    print(min([sum([abs(v - i) for v in s]) for i in range(0, max(s) + 1)]))
 
     # Part 2
-    min_dist = float("inf")
-    for i in range(min(s), max(s) + 1):
-        dist = sum([g(abs(v - i)) for v in s])
-        if dist < min_dist:
-            min_dist = dist
-    print(min_dist)
+    print(min([sum([g(abs(v - i)) for v in s]) for i in range(0, max(s) + 1)]))
